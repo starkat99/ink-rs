28bb4cd423666aba200c408cf4234da94cf60306
Kathryn Long <squeeself@gmail.com>
2018-08-29 23:22:25 -0400

    minimal cli read/write

--- ink/src/lib.rs
+++ ink/src/lib.rs
@@ -1,1 +1,37 @@
 #![feature(uniform_paths)]
+use failure::Fallible;
+use serde_derive::{Deserialize, Serialize};
+use std::io::{Read, Write};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Story {
+    #[serde(rename = "inkVersion")]
+    format_version: u32,
+}
+
+impl Story {
+    const CURRENT_FORMAT_VERSION: u32 = 19;
+    const MIN_FORMAT_VERSION: u32 = 18;
+
+    pub fn new() -> Self {
+        Self {
+            format_version: Self::CURRENT_FORMAT_VERSION,
+        }
+    }
+
+    pub fn from_reader<R: Read>(reader: R) -> Fallible<Self> {
+        Ok(serde_json::from_reader(reader)?)
+    }
+
+    pub fn from_str<S: AsRef<str>>(s: S) -> Fallible<Self> {
+        Ok(serde_json::from_str(s.as_ref())?)
+    }
+
+    pub fn to_writer<W: Write>(&self, writer: W) -> Fallible<()> {
+        Ok(serde_json::to_writer_pretty(writer, self)?)
+    }
+
+    pub fn to_string(&self) -> Fallible<String> {
+        Ok(serde_json::to_string_pretty(self)?)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,108 @@
 #![feature(uniform_paths)]
+use clap::{
+    app_from_crate, crate_authors, crate_description, crate_name, crate_version, AppSettings, Arg,
+};
+use failure::{bail, Fallible};
+use ink::Story;
+use std::{
+    ffi::{OsStr, OsString},
+    fs::File,
+    path::Path,
+};
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Fallible<()> {
+    include_str!("../Cargo.toml"); // Force recompile when manifest is modified
+
+    let args = app_from_crate!()
+        .settings(&[
+            AppSettings::ArgRequiredElseHelp,
+            AppSettings::DontCollapseArgsInUsage,
+            AppSettings::UnifiedHelpMessage,
+        ]).arg(
+            Arg::with_name("input")
+                .takes_value(true)
+                .required(true)
+                .empty_values(false)
+                .value_name("FILE")
+                .validator_os(validate_file_exists)
+                .help("The input ink file"),
+        ).arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .visible_alias("out")
+                .takes_value(true)
+                .empty_values(false)
+                .value_name("COMPILED-FILE")
+                .validator_os(validate_not_dir)
+                .help("Output compiled file"),
+        ).arg(
+            Arg::with_name("compile")
+                .short("c")
+                .long("compile")
+                .help("Validate and compile the input file then exit; does not play file"),
+        ).arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .help("Enable verbose diagnostic messages"),
+        ).arg(
+            Arg::with_name("quiet")
+                .short("q")
+                .long("quiet")
+                .conflicts_with("verbose")
+                .help("Disable all diagnostic messages except errors"),
+        ).get_matches();
+
+    // Initialize logging
+    if args.is_present("verbose") {
+        simple_logging::log_to_stderr(log::LevelFilter::Trace);
+    } else if args.is_present("quiet") {
+        simple_logging::log_to_stderr(log::LevelFilter::Error);
+    } else {
+        simple_logging::log_to_stderr(log::LevelFilter::Info);
+    }
+
+    // Load story
+    let infile = Path::new(args.value_of_os("input").unwrap());
+    let story = if !infile.ends_with(".ink.json") {
+        // TODO: Does not currently support compiling from non-JSON files
+        bail!("ERROR: Compiling .ink files is not implemented yet!");
+    } else {
+        Story::from_reader(File::open(infile)?)?
+    };
+
+    // Write output
+    for &outfile in args.value_of_os("output").map(Path::new).iter() {
+        if outfile != infile {
+            story.to_writer(File::create(outfile)?)?
+        }
+    }
+
+    // Play in interactive mode
+    if !args.is_present("compile") {}
+
+    Ok(())
+}
+
+fn validate_file_exists(s: &OsStr) -> Result<(), OsString> {
+    let path = Path::new(s);
+    if !path.is_file() {
+        let mut err = s.to_os_string();
+        err.push(" is not a valid file");
+        Err(err)
+    } else {
+        Ok(())
+    }
+}
+
+fn validate_not_dir(s: &OsStr) -> Result<(), OsString> {
+    let path = Path::new(s);
+    if path.is_dir() {
+        let mut err = s.to_os_string();
+        err.push(" is a directory");
+        Err(err)
+    } else {
+        Ok(())
+    }
 }
